1ecd31b7377d4458009cc8cc24f0f56abb2b5e60
Olatunde Sodiq <olatundesodiq@gmail.com>
2019-05-31 04:32:34 +0100

    modify admin login

--- admin-login.js
+++ admin-login.js
@@ -1,21 +1,21 @@
 
-    $.ajax({
-        method:"GET",
-        url: "http://localhost:3000/Admin/"
-    })
-    .done(function(responseData){
-        let password = $(".password").val();
-        let email = $(".email").val()
-        console.log(email);
+    // $.ajax({
+    //     method:"GET",
+    //     url: "http://localhost:3000/Admin/"
+    // })
+    // .done(function(responseData){
+    //     let password = $(".password").val();
+    //     let email = $(".email").val()
+    //     console.log(email);
         
-        responseData.forEach(admin => {
-            if(admin.password == password){
-                if(admin.email == email){
-                    window.location = "admin-home.html"
-                }else{
-                    window.location = "admin-login.html"
-                }
-            }
-        });
-    })
+    //     responseData.forEach(admin => {
+    //         if(admin.password == password){
+    //             if(admin.email == email){
+    //                 window.location = "admin-home.html"
+    //             }else{
+    //                 window.location = "admin-login.html"
+    //             }
+    //         }
+    //     });
+    // })
 
